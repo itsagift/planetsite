1c2eb3979563695aab7a2eab6cd87a91c65f9e29
itsagift <anna.c.reynolds@gmail.com>
2021-09-28 17:05:01 -0400

    CSS load images

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ export default function App() {
     <Header setPlanet={setPlanet}/>
     <Planet planet={planet}/>
     <Footer planet={planet}/>
+    <div className="test"></div>
   </div>
   );
 }
